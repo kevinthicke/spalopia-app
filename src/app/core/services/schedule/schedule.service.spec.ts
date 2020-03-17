043ccc48e79ae359d0901d47c966c5eb63b6fbba
kevinthicke <keviincabrera@gmail.com>
2020-03-17 13:18:16 +0100

    Created schedule.service tests

--- src/app/core/services/schedule/schedule.service.spec.ts
+++ src/app/core/services/schedule/schedule.service.spec.ts
@@ -1,40 +1,17 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { ScheduleService } from './schedule.service';
-import { IScheduleModel } from '../../models/schedule.model';
 
 
 const scheduleRawMocked = {
   id: 1,
   spaServiceId: 1,
-  data: [
-    {
-      date: "2020-02-01",
-      ranges: [
-        {
-          start: "10:00",
-          end: "16:00"
-        }
-      ]
-    }
+  data: [ 
+    { date: "2020-02-01", ranges: [{ start: "10:00", end: "16:00" }] }
   ]
 };
 
-const scheduleMocked: IScheduleModel = {
-  id: 1,
-  spaServiceMini: { id: 1, name: null },
-  data: [
-    {
-      date: "2020-02-01",
-      ranges: [
-        {
-          start: "10:00",
-          end: "16:00"
-        }
-      ]
-    }
-  ]
-}
+
 describe('ScheduleService', () => {
 
   let service: ScheduleService;
@@ -57,18 +34,27 @@ describe('ScheduleService', () => {
     expect(service).toBeTruthy();
   });
 
-  xit('should retrieve Schedule by spaServiceId via GET', (done) => {
+  it('should retrieve Schedule by spaServiceId via GET', (done) => {
     
     
     service.getScheduleBySpaServiceId(1).subscribe(schedule => {
+
       expect(schedule).toBeTruthy()
+      expect(schedule).toEqual({
+          id: 1,
+          spaServiceMini: { id: 1, name: null },
+          data: [
+            { date: "2020-02-01", ranges: [{ start: "10:00", end: "16:00" }] }
+          ]
+        });
       done();
+
     });
     
     const request = httpMock.expectOne('http://localhost:3000/schedules?spaServiceId=1');
     
     expect(request.request.method).toBe('GET');
-    request.flush(scheduleMocked);
+    request.flush([ scheduleRawMocked ]);
 
   });
 });
